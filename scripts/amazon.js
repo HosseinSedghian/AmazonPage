284b17f7a77b1ed90339de822cf3ba536a647b80
Hossein Sedghian <HosseinSedghian@gmail.com>
2024-12-29 22:47:00 +0330

    Bug Fixed.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
+
 let finalHTML = '';
   function updateCartQuantity() {
     let cartQuantity = 0;
@@ -30,7 +32,7 @@ let finalHTML = '';
             </div>
 
             <div class="product-price">
-                $${product.priceCents / 100}
+                $${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
