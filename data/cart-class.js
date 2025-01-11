351a2d371b22e1bf437c9e4506eff9495c31bd79
Hossein Sedghian <HosseinSedghian@gmail.com>
2025-01-11 13:12:22 +0330

    cart class created.

---
+++ data/cart-class.js
@@ -0,0 +1,58 @@
+class Cart {
+    cartItems;
+    localstoragekey;
+
+    constructor(locstoragekey) {
+         this.localstoragekey = locstoragekey;
+         this.cartItems = JSON.parse(localStorage.getItem(this.localstoragekey));
+         if (!this.cartItems) {
+            this.cartItems = [];
+         }
+    }
+
+    saveToStorage() {
+        localStorage.setItem(this.localstoragekey, JSON.stringify(this.cartItems));
+    }
+
+    addToCart(productId) {
+        let matchingItem;
+        this.cartItems.forEach((crt) => {
+            if (crt.id === productId) {
+                matchingItem = crt;
+            }
+        });
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        }
+        else {
+            this.cartItems.push({
+                id: cartId,
+                quantity: 1,
+                deliveryOptionId: '1'
+            });
+        }
+        this.saveToStorage();
+    }
+
+    removeFromCart(productId) {
+        let newCart = [];
+        this.cartItems.forEach((item) => {
+            if (item.id !== productId) {
+                newCart.push(item);
+            }
+        });
+        this.cartItems = newCart;
+        saveToStorage();
+    }
+
+    updateDeliveryOptions(productId, deliveryOptionId) {
+        let matchingItem;
+        this.cartItems.forEach((crt) => {
+            if (crt.id === productId) {
+                matchingItem = crt;
+            }
+        });
+        matchingItem.deliveryOptionId = deliveryOptionId;
+        this.saveToStorage();
+    }
+}
